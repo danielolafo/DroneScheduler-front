d233fc71b5282c68c0624c1d9e911db7b147edd1
danielolafo <danielolafo@gmail.com>
2025-03-18 16:23:42 -0500

    Initial state of the project

---
+++ src/drone/DroneScheduler.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export function DroneScheduler() {
+
+    const [drones, setDrones] = useState([])
+
+    useEffect(() => {
+        axios.get('http://droneschedule-dev-env.eba-msy4dwzg.us-east-1.elasticbeanstalk.com/dispatcher/get-available-drones').then((data) => {
+            console.log(data.data.data);
+            setDrones(data?.data?.data);
+        });
+    }, []);
+
+    return (<div>
+        <table class="drone-table">
+            <thead>
+            <tr>
+                <th>Serial number</th>
+                <th>Model</th>
+                <th>Wieght limit</th>
+            </tr>
+            </thead>
+            <tbody>
+                {drones.map((drone, idx) => {
+                    return <tr key={drone.id}>
+                        <td class="drone-cells">{drone.serialNumber}</td>
+                        <td class="drone-cells">{drone.model}</td>
+                        <td class="drone-cells">{drone.weightLimit}</td>
+                    </tr>
+                }
+                )}
+            </tbody>
+        </table>
+    </div>
+    );
+}
